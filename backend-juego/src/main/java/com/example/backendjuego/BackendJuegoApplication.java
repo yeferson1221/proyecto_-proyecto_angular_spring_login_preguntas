fc82fe9b71cb15b0b2c3111aa912fd5a31beeb7c
yeferson.valencia <alejandro.yandd@gmail.com>
2023-01-24 20:20:36 -0500

    iniciando backend correcto 1

---
+++ backend-juego/src/main/java/com/example/backendjuego/BackendJuegoApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendjuego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJuegoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendJuegoApplication.class, args);
+    }
+
+}
---
+++ backend-juego/src/test/java/com/example/backendjuego/BackendJuegoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendjuego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJuegoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
